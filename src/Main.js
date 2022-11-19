4858836b21645ba7db0b031f69e6837c74bdd512
Winijnananas <winijnan.ji@mail.wu.ac.th>
2022-11-19 22:46:24 +0700

    Change Back 7 to 5

--- src/Main.js
+++ src/Main.js
@@ -135,7 +135,7 @@ const styles = StyleSheet.create({
         alignSelf: 'center'
     },
     searchButton: {
-        borderRadius: 5,
+        borderRadius: 7,
         padding: 6,
         backgroundColor: "#3682FF",
         width: "96%",
